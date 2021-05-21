f61220db7f33646caba9c78cd37ff697d24b66d1
Ian Eure <ian@retrospec.tv>
2021-05-20 21:57:34 -0700

    Release v1.2.1

--- exwm-mff.el
+++ exwm-mff.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ian Eure <public@lowbar.fyi>
 ;; URL: https://github.com/ieure/exwm-mff
-;; Version: 1.2.0
+;; Version: 1.2.1
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: unix
 
